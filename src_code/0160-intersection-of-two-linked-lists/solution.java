a0adb8808bffb13b98f79337e6278d29fcc531b2
Praveen Kumar Sharma <dce.pks@gmail.com>
2020-09-02 18:42:14 +0000

    [LeetCode Sync] Runtime - 1 ms (99.91%), Memory - 42.4 MB (100.00%)

---
+++ src_code/0160-intersection-of-two-linked-lists/solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+        ListNode headAPointer = headA;
+        ListNode headBPointer = headB;
+        
+        while(headAPointer != headBPointer){
+            if (headAPointer == null)
+                headAPointer = headB;
+            else
+                headAPointer = headAPointer.next;
+            if (headBPointer == null)
+                headBPointer= headA;
+            else
+                headBPointer = headBPointer.next;
+        }
+        return headAPointer;
+        
+//         HashSet<ListNode> hset = new HashSet<>();
+//         while(headA!=null||headB!=null){
+//             if(headA!=null){
+//                 if(hset.contains(headA)){
+//                    return headA; 
+//                 }
+//                 hset.add(headA);
+//                 headA = headA.next;
+//             }
+//             if(headB!=null){
+//                 if(hset.contains(headB)){
+//                    return headB; 
+//                 }
+//                 hset.add(headB);
+//                 headB = headB.next;
+//             }
+            
+//         }
+//         return null;
+        
+    }
+}
