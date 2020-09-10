339dd61ad25dcb7073d9470c8cf6af2c71f6578e
Praveen Kumar Sharma <dce.pks@gmail.com>
2020-09-10 12:55:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.1 MB (100.00%)

---
+++ src_code/0124-binary-tree-maximum-path-sum/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        max = root.val;
+        maxPathSumHelper(root);
+        return max;
+        
+    }
+    
+    public int maxPathSumHelper(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        
+        int l = maxPathSumHelper(root.left);
+        l = Math.max(l,0);
+        int r = maxPathSumHelper(root.right);
+        r = Math.max(r,0);
+        int maxFromEitherSideToCurr = Math.max(l,r)+root.val;
+        max = Math.max(max,l+r+root.val);
+        return maxFromEitherSideToCurr;
+    }
+}
