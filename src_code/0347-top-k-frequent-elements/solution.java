bfe666d0c1e689ee14c9f316f9eb9e9afcbbcc0c
Praveen Kumar Sharma <dce.pks@gmail.com>
2020-09-12 14:45:24 +0000

    [LeetCode Sync] Runtime - 25 ms (6.13%), Memory - 47.7 MB (82.98%)

---
+++ src_code/0347-top-k-frequent-elements/solution.java
@@ -0,0 +1,67 @@
+class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+
+//         List<Integer>[] bucket = new List[nums.length + 1];
+//         Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+//         for (int n : nums) {
+//             frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+//         }
+
+//         for (int key : frequencyMap.keySet()) {
+//             int frequency = frequencyMap.get(key);
+//             if (bucket[frequency] == null) {
+//                 bucket[frequency] = new ArrayList<>();
+//             }
+//             bucket[frequency].add(key);
+//         }
+
+//         List<Integer> res = new ArrayList<>();
+
+//         for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+//             if (bucket[pos] != null) {
+//                 res.addAll(bucket[pos]);
+//             }
+//         }
+        
+//         int[] result = new int[res.size()];
+//         for(int i=0;i<res.size();i++){
+//             result[i] = res.get(i);
+//         }
+//         return result;
+//     }
+    
+    public int[] topKFrequent(int[] nums, int k) {
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
+        }
+        
+        // 1. build hash map : character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+          count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init heap 'the less frequent element first'
+        Queue<Integer> heap = new PriorityQueue<>(
+            (n1, n2) -> count.get(n1) - count.get(n2));
+
+        // 2. keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: count.keySet()) {
+          heap.add(n);
+          if (heap.size() > k) 
+              heap.poll();    
+        }
+
+        // 3. build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
+        }
+        return top;
+    }
+}
