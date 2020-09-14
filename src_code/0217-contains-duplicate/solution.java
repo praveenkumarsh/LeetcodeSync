18b69c02374756e187e76b3c7afceb6b75f7e187
Praveen Kumar Sharma <dce.pks@gmail.com>
2020-09-14 16:57:09 +0000

    [LeetCode Sync] Runtime - 11 ms (71.05%), Memory - 45.1 MB (100.00%)

---
+++ src_code/0217-contains-duplicate/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int val:nums){
+            if(set.contains(val)){
+                return true;
+            }else{
+                set.add(val);
+            }
+        }
+        return false;
+        
+    }
+}
