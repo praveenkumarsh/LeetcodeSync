7a560f7370408da010790bd6f19f0eb748bc3d94
Praveen Kumar Sharma <dce.pks@gmail.com>
2021-03-10 02:39:40 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 41.7 MB (100.00%)

--- src_code/0200-number-of-islands/solution.java
+++ src_code/0200-number-of-islands/solution.java
@@ -1,29 +1,41 @@
 class Solution {
     
-    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int n;
+    int m;
     
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) 
+        int c  = 0;
+        n = grid.length;
+        m = grid[0].length;
+        if(n==0){
             return 0;
+        }
         
-        int cnt = 0, m = grid.length, n = grid[0].length;
-        
-        for (int i = 0; i < m; i++) 
-            for (int j = 0; j < n; j++) 
-                if (grid[i][j] == '1') {
-                    DFS (grid, i, j, m, n);
-                    cnt++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    mark(grid,i,j);
+                    c++;
                 }
-        return cnt;
+            }
+        }
+        return c;
+        
+        
+        
     }
     
-    private void DFS (char[][] grid, int i, int j, int m, int n) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') 
+    public void mark(char[][] grid , int i, int j){
+        if(i<0|| j<0||i>=n ||j>=m||grid[i][j]!='1'){
             return;
+        }
         
-        grid[i][j] = '0';
+
+        grid[i][j]= '0';
+        mark(grid,i+1,j);
+        mark(grid,i,j+1);
+        mark(grid,i-1,j);
+        mark(grid,i,j-1);
         
-        for (int[] dir : dirs)  
-            DFS(grid, i + dir[0], j + dir[1], m, n); 
     }
 }
