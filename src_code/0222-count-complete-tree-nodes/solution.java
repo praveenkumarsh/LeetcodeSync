cef9cb97094468e6e82b0d76ce16718280a5f232
Praveen Kumar Sharma <dce.pks@gmail.com>
2023-10-22 19:29:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.7 MB (99.90%)

---
+++ src_code/0222-count-complete-tree-nodes/solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+	public int countNodes(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		int leftDepth = getLeftDepth(root);
+		int rightDepth = getRightDepth(root);
+
+		if (leftDepth == rightDepth) {
+			return (1 << leftDepth) - 1;
+		} else {
+			return 1 + countNodes(root.left) + countNodes(root.right);
+		}
+	}
+
+	private int getLeftDepth(TreeNode node) {
+		int depth = 0;
+		while (node != null) {
+			depth++;
+			node = node.left;
+		}
+		return depth;
+	}
+
+	private int getRightDepth(TreeNode node) {
+		int depth = 0;
+		while (node != null) {
+			depth++;
+			node = node.right;
+		}
+		return depth;
+	}
+}
