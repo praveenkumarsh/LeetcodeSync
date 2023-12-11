b38f886f33f03c749cba94d94ad05d6a1fadd611
Praveen Kumar Sharma <dce.pks@gmail.com>
2023-12-11 17:55:56 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 47.6 MB (99.99%)

--- src_code/0200-number-of-islands/solution.java
+++ src_code/0200-number-of-islands/solution.java
@@ -1,41 +1,42 @@
 class Solution {
-    
-    int n;
-    int m;
-    
+    int row, column, count;
+    char[][] arr;
+    private static char LAND_AREA = '1';
+
     public int numIslands(char[][] grid) {
-        int c  = 0;
-        n = grid.length;
-        m = grid[0].length;
-        if(n==0){
-            return 0;
+        row = grid.length;
+        column = grid[0].length;
+        arr = grid;
+
+        count = 0;
+        for(int i = 0; i < row; i++) {
+            check(grid[i],i);
         }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    mark(grid,i,j);
-                    c++;
-                }
+        return count;
+    }
+
+    private void check(char[] arr_row,int arr_column) {
+        for(int j = 0; j < column; j++) {
+            if(LAND_AREA == arr_row[j]) {
+                hasLand(arr_column, j);
+                count++;
             }
         }
-        return c;
-        
-        
-        
     }
-    
-    public void mark(char[][] grid , int i, int j){
-        if(i<0|| j<0||i>=n ||j>=m||grid[i][j]!='1'){
-            return;
-        }
-        
 
-        grid[i][j]= '0';
-        mark(grid,i+1,j);
-        mark(grid,i,j+1);
-        mark(grid,i-1,j);
-        mark(grid,i,j-1);
-        
+    private void hasLand(int i, int j){
+        arr[i][j] = 2;
+        if(i - 1 >= 0 && arr[i-1][j] == LAND_AREA) {
+            hasLand(i-1, j);
+        }
+        if(i + 1 < row && arr[i+1][j] == LAND_AREA) {
+            hasLand(i+1, j);
+        }
+        if(j - 1 >= 0 && arr[i][j-1] == LAND_AREA) {
+            hasLand(i, j-1);
+        }
+        if(j + 1 < column && arr[i][j+1] == LAND_AREA) {
+            hasLand(i, j+1);
+        }
     }
 }
