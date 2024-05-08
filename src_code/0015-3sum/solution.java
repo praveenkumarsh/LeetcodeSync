fa08db364e9a0493f08501f828773c1d1be3c21d
Praveen Kumar Sharma <dce.pks@gmail.com>
2024-05-08 18:05:42 +0000

    [LeetCode Sync] Runtime - 29 ms (73.09%), Memory - 51.2 MB (76.28%)

--- src_code/0015-3sum/solution.java
+++ src_code/0015-3sum/solution.java
@@ -1,30 +1,33 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        
-        for(int i = 0; i < nums.length-2; i++) {
-            if(i > 0 && (nums[i] == nums[i-1])) 
-                continue; // avoid duplicates
-            for(int j = i+1, k = nums.length-1; j<k;) {
-                if(nums[i] + nums[j] + nums[k] == 0) {
-                    list.add(Arrays.asList(nums[i],nums[j],nums[k]));
+    public List<List<Integer>> threeSum(int[] arr) {
+        int n = arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            //remove duplicates:
+            if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+            //moving 2 pointers:
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                    ans.add(temp);
                     j++;
                     k--;
-                    
-                    while((j < k) && (nums[j] == nums[j-1]))
-                        j++;// avoid duplicates
-                    while((j < k) && (nums[k] == nums[k+1]))
-                        k--;// avoid duplicates
-                }else if(nums[i] + nums[j] + nums[k] > 0){
-                    k--;
-                }else{
-                    j++;
+                    //skip the duplicates:
+                    while (j < k && arr[j] == arr[j - 1]) j++;
+                    while (j < k && arr[k] == arr[k + 1]) k--;
                 }
             }
         }
-        
-        return list;
+        return ans;
     }
 }
