1fa458c2959e1873d736dc5f1d67a9a17168d7cc
Praveen Kumar Sharma <dce.pks@gmail.com>
2024-05-08 17:44:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44 MB (95.63%)

--- src_code/0138-copy-list-with-random-pointer/solution.java
+++ src_code/0138-copy-list-with-random-pointer/solution.java
@@ -14,50 +14,34 @@ class Node {
 */
 
 class Solution {
-   public Node copyRandomList(Node head) {
-      if(head == null){
-          return head;
-    }
-    
-    Node node = head;
-    
-    //head: 1 - 2 - 3 - 4
-    
-    //First loop, add copy after node directly
-    while(node != null){
-        Node next = node.next; //so in the example, in first loop, next now is 2
-        node.next = new Node(node.val); //in first loop, node.next now is 1'
-        node.next.next = next; //so in first loop, it is 1 - 1' - 2now
-        node = next; //in first loop, for next node, 2
-    }
-    
-    //After loop, it is currently, 1 - 1' - 2 - 2' - 3 - 3' - 4 - 4'
-    
-    //Second loop, link random for copyhead
-    node = head;
-    while(node != null){
-        if(node.random != null){
-            node.next.random = node.random.next; //in first loop, it will be 1' = 3'
+    public Node copyRandomList(Node head) {
+        Node temp = head;
+        //step 1
+        while(temp != null) {
+            Node newNode = new Node(temp.val);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = temp.next.next;
         }
-        node = node.next.next; //in first loop, it will be 2
-    }
-    
-    //Third loop, divide into head and copyhead
-    node = head;
-    Node copyHead = node.next;  //it is 1' initially
-    Node copy = copyHead; //it is 1' initially
-    while(copy.next != null){
-        //first copy.next is 2
-        node.next = node.next.next; //In first loop, node.next now is 2
-        node = node.next; //In first loop, node now is 2
-        
-        copy.next = copy.next.next; //In first loop, copy.next is 2'
-        copy = copy.next; //In first loop, copy is 2'
-        
-    }
-    //Last step
-    node.next = node.next.next; 
-    
-    return copyHead;
- }
+        //step 2
+        Node itr = head;
+        while(itr != null) {
+            if(itr.random != null)
+                itr.next.random = itr.random.next;
+            itr = itr.next.next;
+        }
+        //step 3
+        Node dummy = new Node(0);
+        itr = head;
+        temp = dummy;
+        Node fast;
+        while(itr != null) {
+            fast = itr.next.next;
+            temp.next = itr.next;
+            itr.next = fast;
+            temp = temp.next;
+            itr = fast;
+        }
+        return dummy.next;
+}
 }
