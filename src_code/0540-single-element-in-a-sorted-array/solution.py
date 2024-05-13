31419b25c9a577e85ac4b79e69361715a24c2a72
Praveen Kumar Sharma <dce.pks@gmail.com>
2024-05-13 13:34:03 +0000

    [LeetCode Sync] Runtime - 121 ms (82.26%), Memory - 18.6 MB (95.82%)

---
+++ src_code/0540-single-element-in-a-sorted-array/solution.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def singleNonDuplicate(self, arr):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(arr)  # Size of the array
+
+        # Edge cases:
+        if n == 1:
+            return arr[0]
+        if arr[0] != arr[1]:
+            return arr[0]
+        if arr[n - 1] != arr[n - 2]:
+            return arr[n - 1]
+
+        low = 1
+        high = n - 2
+        while low <= high:
+            mid = (low + high) // 2
+
+            # If arr[mid] is the single element:
+            if arr[mid] != arr[mid + 1] and arr[mid] != arr[mid - 1]:
+                return arr[mid]
+
+            # We are in the left:
+            if (mid % 2 == 1 and arr[mid] == arr[mid - 1]) or (mid % 2 == 0 and arr[mid] == arr[mid + 1]):
+                # Eliminate the left half:
+                low = mid + 1
+            # We are in the right:
+            else:
+                # Eliminate the right half:
+                high = mid - 1
+
+        # Dummy return statement:
+        return -1
+            
