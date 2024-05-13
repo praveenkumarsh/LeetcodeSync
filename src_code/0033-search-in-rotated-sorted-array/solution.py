b1105177865a3d6bf2d49d731aa48014084f92aa
Praveen Kumar Sharma <dce.pks@gmail.com>
2024-05-13 13:38:56 +0000

    [LeetCode Sync] Runtime - 18 ms (88.52%), Memory - 11.9 MB (62.11%)

---
+++ src_code/0033-search-in-rotated-sorted-array/solution.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def search(self, arr, k):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(arr)
+        low = 0
+        high = n - 1
+        while low <= high:
+            mid = (low + high) // 2
+
+            # if mid points the target
+            if arr[mid] == k:
+                return mid
+
+            # if left part is sorted
+            if arr[low] <= arr[mid]:
+                if arr[low] <= k and k <= arr[mid]:
+                    # element exists
+                    high = mid - 1
+                else:
+                    # element does not exist
+                    low = mid + 1
+            else:  # if right part is sorted
+                if arr[mid] <= k and k <= arr[high]:
+                    # element exists
+                    low = mid + 1
+                else:
+                    # element does not exist
+                    high = mid - 1
+        return -1
+            
