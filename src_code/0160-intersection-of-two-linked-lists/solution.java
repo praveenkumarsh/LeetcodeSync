8dfa894b509ae8a6b2b5127ef6326a5eed73da4f
Praveen Kumar Sharma <dce.pks@gmail.com>
2024-08-18 21:14:28 +0000

    [LeetCode Sync] Runtime - 1 ms (99.90%), Memory - 48.3 MB (73.93%)

--- src_code/0160-intersection-of-two-linked-lists/solution.java
+++ src_code/0160-intersection-of-two-linked-lists/solution.java
@@ -1,27 +1,22 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
-    //utility function to check presence of intersection
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode d1 = headA;
         ListNode d2 = headB;
-        
-        while(d1 != d2) {
-            d1 = d1 == null? headB:d1.next;
-            d2 = d2 == null? headA:d2.next;
-
-            System.out.println("===== d1 : "+ (d1 == null ? null : d1.val) + " | d2 : " + (d2 == null ? null : d2.val));
+        while (d1 != d2) {
+            d1 = d1 == null ? headB : d1.next;
+            d2 = d2 == null ? headA : d2.next;
         }
-        
         return d1;
     }
 }
